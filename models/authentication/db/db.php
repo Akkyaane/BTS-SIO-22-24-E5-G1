30652a745cb4452bd481157f63c290f0c01548b3
skid <nitroxxnitroxx42130@gmail.com>
2023-11-10 17:06:08 +0100

    Add an interface for Expense Sheets

--- models/authentication/db/db.php
+++ models/authentication/db/db.php
@@ -1,21 +1,21 @@
 <?php
 
-// MAMP
- try {
- $db_connect = new PDO('mysql:host=localhost:8889; dbname=db_gsb; charset=utf8', 'root', 'root');
-  echo "Connexion réussie. ";
- }
+//MAMP
+ //try {
+ //$db_connect = new PDO('mysql:host=localhost:8889; dbname=db_gsb; charset=utf8', 'root', 'root');
+  //echo "Connexion réussie. ";
+ //}
 
- catch (Exception $e) {
- die('Erreur : ' . $e->getMessage());
- }
+ //catch (Exception $e) {
+ //die('Erreur : ' . $e->getMessage());
+ //}
 
-// WAMP
-// try {
-//     $db_connect = new PDO('mysql:host=localhost:3306; dbname=db_gsb; charset=utf8', 'root', '');
-//     //  echo "Connexion réussie. ";
-// } catch (Exception $e) {
-//     die('Erreur : ' . $e->getMessage());
-// }
+//WAMP
+try {
+    $db_connect = new PDO('mysql:host=localhost:3306; dbname=db_gsb; charset=utf8', 'root', '');
+     echo "Connexion réussie. ";
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 
 ?>
