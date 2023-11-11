f8d8e056082ee28ee6760d9a867d13d62c335781
Jérémie <bj.06202000@gmail.com>
2023-11-11 16:32:53 +0100

    Make changes in the 'AddExpenseSheet' file

--- views/visitor/documents/addExpenseSheet.php
+++ views/visitor/documents/addExpenseSheet.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GSB - Ajout d'une fiche de frais</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="addExpenseSheet.css">
 </head>
 <body>
     <header class="bg-primary text-white text-center py-4">
@@ -19,37 +20,47 @@ session_start();
             <p>Pour chaque catégorie, veuillez inscrire le montant total des dépenses et fournir les justificatifs nécessaires.</p>
             <form action="../../../models/expenseSheet/addExpenseSheet.php" method="post">
                 <div class="mb-3">
-                    <h3>Informations personnelles</h3>
-                    <input type="text" class="form-control" name="last_name" placeholder="Nom" required>
-                </div>
-                <div class="mb-3">
-                    <input type="text" class="form-control" name="first_name" placeholder="Prénom" required>
-                </div>
-                <div class="mb-3">
-                    <input type="email" class="form-control" name="email" placeholder="E-mail" required>
-                </div>
-                <div class="mb-3">
-                    <h3>Informations supplémentaires</h3>
+                    <h3>Informations générales</h3>
                     <label for="date">Date de départ :</label>
-                    <input type="date" class="form-control" name="start_date" placeholder="Date" required>
+                    <input type="date" class="form-control" name="start_date" required>
                 </div>
                 <div class="mb-3">
                     <label for="date">Date de retour :</label>
-                    <input type="date" class="form-control" name="end_date" placeholder="Date" required>
+                    <input type="date" class="form-control" name="end_date" required>
+                </div>
+                <div class="mb-3">
+                    <input type="number" class="form-control" name="nights_number" placeholder="Nombre de nuitées" required>
                 </div>
                 <div class="mb-3">
                     <label for="date">Date de la demande de soumission :</label>
-                    <input type="date" class="form-control" name="request_date" placeholder="Date" required>
+                    <input type="date" class="form-control" name="request_date" required>
                 </div>
                 <div class="mb-3">
                     <h3>Frais</h3>
                 </div>
                 <div class="mb-3">
                     <h5>Transport</h5>
-                    <input type="number" step=0.01 class="form-control" name="transport_expense" placeholder="Montant total en euros">
+                    <select class="form-select" id="transport_category" onchange="showDiv(this)" required>
+                        <option hidden selected>Sélectionnez le type de transport</option>
+                        <option value="1">Avion</option>
+                        <option value="2">Bus</option>
+                        <option value="3">Car</option>
+                        <option value="4">Taxi</option>
+                        <option value="5">Train</option>
+                        <option value="6">Voiture</option>
+                    </select>
                 </div>
                 <div class="mb-3">
-                    <input type="file" class="form-control" name="transport_expense_file">
+                    <input type="number" step=0.01 class="form-control form-control-v2" id="mileage_expenses" name="mileage_expenses" placeholder="Nombre total de kilomètres">
+                </div>
+                <div class="mb-3">
+                    <input type="number" step=0.01 class="form-control form-control-v2" id="tax_horsepower" name="tax_horsepower" placeholder="Nombre de chevaux">
+                </div>
+                <div class="mb-3">
+                    <input type="number" step=0.01 class="form-control form-control-v2" id="transport_expense" name="transport_expense" placeholder="Montant total en euros">
+                </div>
+                <div class="mb-3">
+                    <input type="file" class="form-control form-control-v2" id="transport_expense_file" name="transport_expense_file">
                 </div>
                 <div class="mb-3">
                     <h5>Hébergement</h5>
@@ -64,13 +75,6 @@ session_start();
                 </div>
                 <div class="mb-3">
                     <input type="file" class="form-control" name="food_expense_file">
-                </div>
-                <div class="mb-3">
-                    <h5>Évènements</h5>
-                    <input type="number" step=0.01 class="form-control" name="events_expense" placeholder="Montant total en euros">
-                </div>
-                <div class="mb-3">
-                    <input type="file" class="form-control" name="events_expense_file">
                 </div>
                 <div class="mb-3">
                     <h5>Autres</h5>
@@ -93,6 +97,21 @@ session_start();
                     const remainingChars = textarea.value.length;
                     charCount.textContent = `${remainingChars}/500`;
                     });
+
+                    function showDiv(select){
+                        if(select.value== 1 || 2 || 3 || 4 || 5){
+                            document.getElementById('mileage_expenses').style.display = "none";
+                            document.getElementById('tax_horsepower').style.display = "none";
+                            document.getElementById('transport_expense').style.display = "block";
+                            document.getElementById('transport_expense_file').style.display = "block";
+                        }
+                        if (select.value== 6) {
+                            document.getElementById('mileage_expenses').style.display = "block";
+                            document.getElementById('tax_horsepower').style.display = "block";
+                            document.getElementById('transport_expense').style.display = "none";
+                            document.getElementById('transport_expense_file').style.display = "none";
+                        };
+                    };
                 </script>
             </form>
         </div>
